205e170eba4f95deea518eee77b374635fcd1993
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-03-25 16:46:24 +0100

    first interface of Maki, with stub of implem

--- src/maki.ml
+++ src/maki.ml
@@ -2,3 +2,189 @@
 (* This file is free software. See file "license" for more details. *)
 
 (** {1 Maki: Persistent Incremental Computations} *)
+
+open Result
+
+module B = Bencode
+
+type 'a or_error = ('a, string) Result.result
+type 'a lwt_or_error = 'a or_error Lwt.t
+type ('a,'rw) pipe = ('a,'rw) Maki_pipe.t
+
+module Res_ = struct
+  let (>|=) r f = match r with
+    | Ok x -> Ok (f x)
+    | Error e -> Error e
+end
+
+(** {2 Basic types} *)
+
+type path = string
+type program = string
+
+(** {2 Values} *)
+module Value = struct
+  type 'a ops = {
+    descr: string; (* description of 'a *)
+    serialize : 'a -> Bencode.t;
+    unserialize : Bencode.t -> 'a or_error;
+  }
+
+  type t = Value : 'a ops * 'a -> t
+  (** Universal type, can be hashed, serialized, etc. *)
+
+  let expected_s what s = Error (Printf.sprintf "expected %s, got %s" what s)
+  let expected_b what b = expected_s what (B.encode_to_string b)
+
+  let int = {
+    descr="int";
+    serialize=(fun i -> B.Integer i);
+    unserialize=(function
+      | B.Integer i -> Ok i
+      | B.String s ->
+        begin
+          try Ok (int_of_string s)
+          with _ -> expected_s "int" s
+        end;
+      | b -> expected_b "int" b
+      );
+  }
+ 
+  let string = {
+    descr="string";
+    serialize=(fun s -> Bencode.String s);
+    unserialize=(function
+        | B.String s -> Ok s
+        | b -> expected_b "string" b
+      );
+  }
+
+  let bool = {
+    descr="bool";
+    serialize=(fun b -> if b then B.Integer 1 else B.Integer 0);
+    unserialize=(function
+          | B.Integer 0 -> Ok false
+          | B.Integer n when n<>0 -> Ok true
+          | B.String s ->
+            begin try Ok (bool_of_string s)
+              with _ -> expected_s "bool" s
+            end
+          | b -> expected_b "bool" b
+        );
+  }
+
+  (* FIXME: behavior is different, comparison should be by timestamp+hash *)
+  let file = string
+
+  exception ExitUn of string
+
+  let map_res_l f l =
+    try 
+      let res =
+        List.map
+        (fun x -> match f x with Ok y -> y | Error s -> raise (ExitUn s))
+        l
+      in
+      Ok res
+    with ExitUn s -> Error s
+
+  let list op =
+    let descr = "list " ^ op.descr in
+    { descr;
+      serialize=(fun l -> B.List (List.map op.serialize l));
+      unserialize=(function
+          | B.List l -> map_res_l op.unserialize l
+          | b -> expected_b descr b
+        );
+    }
+
+  let assoc op =
+    let descr = "assoc " ^ op.descr in
+    { descr;
+      serialize=(fun l -> B.Dict (List.map (fun (name,x) -> name, op.serialize x) l));
+      unserialize=(function
+          | B.Dict l ->
+            map_res_l
+              (fun (name,x) -> Res_.(op.unserialize x >|= fun x -> name, x))
+              l
+          | b -> expected_b descr b
+        );
+  }
+
+  let pair a b = assert false
+  let triple a b c = assert false
+  let quad a b c d = assert false
+
+  let serialize op x = op.serialize x
+  let unserialize op x = op.unserialize x
+  let to_string op x = B.encode_to_string (serialize op x)
+  let of_string op s =
+    try
+      let b = B.decode (`String s) in
+      unserialize op b
+    with e -> Error (Printexc.to_string e)
+  let pack op x = Value (op,x)
+end
+
+(** {2 Controlling Parallelism} *)
+
+module J = struct
+  let j_ = ref 1
+
+  let get () = !j_
+  let set n =
+    if n<1 then invalid_arg "J.set";
+    j_ := n
+
+  (* pool used to limit concurrent access to cores, memory, etc. *)
+  let pool_ =
+    lazy (Lwt_pool.create !j_ (fun () -> Lwt.return_unit))
+
+  let acquire f = Lwt_pool.use (Lazy.force pool_) f
+end
+
+(** {2 On-Disk storage} *)
+module Storage = struct
+  type t = {
+    name: string;
+    get: string -> string or_error;
+    set: string -> string -> unit or_error;
+    iter: unit -> (string * string, [`r]) Maki_pipe.t;
+  }
+
+  let env_var_ = "MAKI_DIR"
+
+  (* TODO *)
+
+  let default_get ~dir k = assert false
+  let default_set ~dir k v = assert false
+  let default_iter ~dir () = assert false
+
+  let default ?dir () =
+    let dir = match dir with
+      | Some d -> d
+      | None ->
+        try Sys.getenv env_var_
+        with Not_found -> "."
+    in
+    { name="shelf";
+      get=default_get ~dir;
+      set=default_set ~dir;
+      iter=default_iter ~dir;
+    }
+
+  let storage_ = ref (default ())
+  let set s = storage_ := s
+  let cur () = !storage_
+end
+
+(** {2 Memoized Functions} *)
+
+let call
+    ?(storage=Storage.cur ())
+    ~name
+    ~deps
+    ~op
+    f
+  =
+  assert false (* TODO *)
--- src/maki.mli
+++ src/maki.mli
@@ -3,4 +3,110 @@
 
 (** {1 Maki: Persistent Incremental Computations} *)
 
+type 'a or_error = ('a, string) Result.result
+type 'a lwt_or_error = 'a or_error Lwt.t
+type ('a,'rw) pipe = ('a,'rw) Maki_pipe.t
+
+(** {2 Basic types} *)
+
+type path = string
+type program = string
+
+(** {2 Values}
+
+    Maki deals with functions that take and return values of any
+    type that provides a {!'a Value.t} implementation;
+    that is, values that we can serialize, unserialize, and hash.
+    The reason is that values are stored on disk for memoization purposes. *)
+module Value : sig
+  type 'a ops = {
+    descr: string; (* description of 'a *)
+    serialize : 'a -> Bencode.t;
+    unserialize : Bencode.t -> 'a or_error;
+  }
+  (* TODO: also explain how to hash/compare values
+     (e.g. with a to_string_canon function, which, for files, will
+     be the hash of the content) *)
+
+  type t = Value : 'a ops * 'a -> t
+  (** Universal type, can be hashed, serialized, etc. *)
+
+  val int : int ops
+  val string : string ops
+  val bool : bool ops
+  val list : 'a ops -> 'a list ops
+  val assoc : 'a ops -> (string * 'a) list ops
+
+  val file : path ops
+  (** A {b reference} to some file content. This should be compared by
+      hash of the file content *)
+
+  val pair : 'a ops -> 'b ops -> ('a * 'b) ops
+  val triple : 'a ops -> 'b ops -> 'c ops -> ('a * 'b * 'c) ops
+  val quad : 'a ops -> 'b ops -> 'c ops -> 'd ops -> ('a * 'b * 'c * 'd) ops
+
+  val serialize : 'a ops -> 'a -> Bencode.t
+  val unserialize : 'a ops -> Bencode.t -> 'a or_error
+  val to_string : 'a ops -> 'a -> string
+  val of_string : 'a ops -> string -> 'a or_error
+  val pack : 'a ops -> 'a -> t
+end
+
+(** {2 On-Disk storage}
+
+    We use a generic interface for on-disk storage, in the form of a
+    dictionary [string -> string]. The default storage just uses
+    one file per pair. *)
+module Storage : sig
+  type t = {
+    name: string;
+    get: string -> string or_error;
+    set: string -> string -> unit or_error;
+    iter: unit -> (string * string, [`r]) Maki_pipe.t;
+  }
+
+  val default : ?dir:path -> unit -> t
+  (** [default ?dir ()] creates a new default storage (one file per pair)
+      @param dir if provided, set the directory used for storing files
+      if [dir] is not set, then the current directory is used, unless the
+        environment variable "MAKI_DIR" is set *)
+
+  val set : t -> unit
+  (** Change the storage that is used to evaluate every Maki function *)
+end
+
+(** {2 Controlling Parallelism} *)
+
+module J : sig
+  val set : int -> unit
+  val get : unit -> int
+  val acquire : (unit -> 'a Lwt.t) -> 'a Lwt.t
+end
+
+(** {2 Memoized Functions}
+
+    This is the heart of the library: a wrapper around {b pure} functions
+    from {!Value} arguments to a {!Value}-aware type. Such functions
+    are supposed to always return the same value given the same arguments
+    and dependencies (a typical dependency is an external program that
+    might have several version).
+
+    The {!call} function is used to actually evaluate a wrapped function,
+    or return its memoized result if the computation was done already.
+
+    We need to name functions because, from one execution to another,
+    the results of a function call must be stored on disk. Names are used
+    to map function calls to their result. If two different functions
+    share the same name (even across programs), the results will be
+    unpredictable.
+*)
+
+val call :
+  ?storage:Storage.t ->
+  name:string ->
+  deps:Value.t list ->
+  op:'res Value.ops ->
+  (unit -> 'res Lwt.t) ->
+  'res Lwt.t
+
 
